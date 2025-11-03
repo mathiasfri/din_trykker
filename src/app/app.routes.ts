e260f477c01f83080f68b3fdb6aae9fadc984c6b
mathiasfri <mathiasfridthjof@live.dk>
2025-11-04 00:27:52 +0100

    lazy load & budget raise

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,17 @@
 import { Routes } from '@angular/router';
-import { AppShellComponent } from './app.shell';
 
 export const routes: Routes = [
   {
     path: '',
-    component: AppShellComponent,
+    loadComponent: () => import('./app.shell').then(m => m.AppShellComponent),
     children: [
-      {
-        path: '',
-        loadComponent: () => import('./features/home/home.component').then((m) => m.HomeComponent),
-      },
-      {
-        path: 'om',
-        loadComponent: () =>
-          import('./features/about/about.component').then((m) => m.AboutComponent),
-      },
-      {
-        path: 'kontakt',
-        loadComponent: () =>
-          import('./features/contact/contact.component').then((m) => m.ContactComponent),
-      },
-      {
-        path: 'trykmetoder/dtf',
-        loadComponent: () =>
-          import('./features/print-methods/print-methods.component').then(
-            (m) => m.PrintMethodsComponent
-          ),
-      },
-      {
-        path: 'tilbud',
-        loadComponent: () =>
-          import('./features/offers/offers.component').then((m) => m.OffersComponent),
-      },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
 
+      { path: 'home', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
+      { path: 'om', loadComponent: () => import('./features/about/about.component').then(m => m.AboutComponent) },
+      { path: 'kontakt', loadComponent: () => import('./features/contact/contact.component').then(m => m.ContactComponent) },
+      { path: 'trykmetoder/dtf', loadComponent: () => import('./features/print-methods/print-methods.component').then(m => m.PrintMethodsComponent) },
+      { path: 'tilbud', loadComponent: () => import('./features/offers/offers.component').then(m => m.OffersComponent) },
     ],
   },
   { path: '**', redirectTo: '' },
